e486d95d66372c371ab31b626413d1156f3e8694
buket.culfaoglu <buket.culfaoglu@prestijyazilim.com.tr>
2021-10-22 14:53:55 +0300

    cors fix

--- comments/index.js
+++ comments/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const { randomBytes } = require('crypto');
+const cors = require('cors');
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cors());
 
 const commentsByPostId = {};
 
--- posts/index.js
+++ posts/index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 //const bodyParser = require('body-parser');
 const { randomBytes } = require('crypto');
+const cors = require('cors');
+
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-
+app.use(cors());
 //app.use(bodyParser.json());
 
 const posts = {};
