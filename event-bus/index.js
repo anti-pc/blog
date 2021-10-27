74d72a13f3fa32b6193f57438e92fc96638a721f
buket.culfaoglu <buket.culfaoglu@prestijyazilim.com.tr>
2021-10-27 18:24:07 +0300

    parsing incoming events

--- event-bus/index.js
+++ event-bus/index.js
@@ -1,25 +1,22 @@
 const express = require("express");
 const axios = require("axios");
-//const { randomBytes } = require('crypto');
-//const cors = require('cors');
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-// app.use(cors());
-
-// app.get('/events/:id/comments', (req, res) =>
-// {
-//     res.send(commentsByPostId[req.params.id] || []);
-// });
 
 app.post("/events", (req, res) => {
   const event = req.body;
 
-  axios.post("http://localhost:4000/events", event);
-  axios.post("http://localhost:4001/events", event);
-  axios.post("http://localhost:4002/events", event);
-
+  axios.post("http://localhost:4000/events", event).catch((err) => {
+    console.log(err.message);
+  });
+  axios.post("http://localhost:4001/events", event).catch((err) => {
+    console.log(err.message);
+  });
+  axios.post("http://localhost:4002/events", event).catch((err) => {
+    console.log(err.message);
+  });
   res.send({ status: "OK" });
 });
 
